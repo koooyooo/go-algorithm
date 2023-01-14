2c315c98dfff9a79c1fb3fe72630c7ad958fc217
koooyooo <jab.cross.hook@gmail.com>
2023-01-15 02:13:14 +0900

    enhance comment

--- graph/breadth_first_search.go
+++ graph/breadth_first_search.go
@@ -16,13 +16,13 @@ func handleQueue(graph map[int]*distancePoint, queue []*distancePoint) {
 	for len(queue) > 0 {
 		head := queue[0]
 		head.visited = true
-		queue = queue[1:]
+		queue = queue[1:] // Queueは計算対象なので、自身は除外しておく
 		for _, idx := range head.neighbors {
 			neighbor := graph[idx]
-			if neighbor.visited {
+			if neighbor.visited { // 処理済のPointは追加自体をしない（追加後に処理をしないロジックだと 空の仕事がQueueにたまり続けて無限ループになる）
 				continue
 			}
-			neighbor.distance = head.distance + 1
+			neighbor.distance = head.distance + 1 // 親の値が見えるこの時点でインクリメント
 			queue = append(queue, neighbor)
 		}
 	}
