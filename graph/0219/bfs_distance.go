b710ed87daa4def49f537a2f2ed45c817eaefd94
koooyooo <jab.cross.hook@gmail.com>
2023-02-23 12:43:23 +0900

    update comments

--- graph/0219/bfs_distance.go
+++ graph/0219/bfs_distance.go
@@ -3,26 +3,31 @@ package _219
 // [幅優先探索]
 // スタート地点からの距離を測定
 func fillDistance(g map[int][]int, s int) map[int]int {
-	// 距離と発見済みマークを初期化
+	// ① 拡張点の距離・発見済Mapを初期化・開始地点を設定
 	dist := map[int]int{s: 0}
 	seen := map[int]bool{s: true}
 
+	// ② Queueを初期化・開始地点を設定
 	queue := []int{s}
 
+	// ③ Queueの要素でループ（終了条件 = Queue要素なし）
 	for len(queue) > 0 {
-		// 先頭要素をPOP
+
+		// ④ 先頭要素をPOP
 		head := queue[0]
 		queue = queue[1:]
 
+		// ⑤ 先頭要素の周囲を探索
 		for _, n := range g[head] {
-			// 発見済の場合はキャンセル（Queue追加による無限ループ予防）
+			// ⑥ 発見済の頂点はキャンセル（Queue追加による無限ループ予防）
 			if seen[n] {
 				continue
 			}
-			// 実処理
+			// ⑦ 実処理（距離測定）
 			dist[n] = dist[head] + 1 // 親の値が見えるこの時点でインクリメント
 
-			// 発見済みとしてマークした上で Queueに追加（①未発見 ②発見済-Queue処理待ち ③発見済-Queue処理済 …のステップにおける②）
+			// ⑧ 頂点を発見済みとしてマークして Queueに追加
+			//  （①未発見 ②発見済-Queue処理待ち ③発見済-Queue処理済 …のステップにおける②）
 			seen[n] = true
 			queue = append(queue, n)
 		}
--- graph/0219/dfs_connected.go
+++ graph/0219/dfs_connected.go
@@ -2,10 +2,11 @@ package _219
 
 // グラフの連結を確認する
 func connected(g map[int][]int, s int) bool {
+	// ① 訪問履歴 Mapを初期化
 	visited := make(map[int]bool)
-	// スタート地点から走査する
+	// ② スタート地点から再帰的に走査
 	visit(g, visited, s)
-	// visitedフラグの無い頂点があれば未到達ありと判断
+	// ③ 結果確認（訪問履歴の無い頂点が残っているか）
 	for v, _ := range g {
 		if !visited[v] {
 			return false
@@ -16,8 +17,11 @@ func connected(g map[int][]int, s int) bool {
 
 // 目標がリストに無ければ一段回戻るロジックは再起で表現できる
 func visit(g map[int][]int, visited map[int]bool, current int) {
+	// ④ 訪問履歴 を記載
 	visited[current] = true
+	// ⑤ 周囲の頂点を再帰的に検索
 	for _, p := range g[current] {
+		// ⑥ 訪問履歴があればスキップ
 		if visited[p] {
 			continue
 		}
